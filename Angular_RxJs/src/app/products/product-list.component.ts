3f37902688b02c86c291f4d332c53715d3efe01a
jeffrey grospe <jeffreygrospe@yahoo.com>
2023-11-19 21:02:09 -0500

    add new project

---
+++ Angular_RxJs/src/app/products/product-list.component.ts
@@ -0,0 +1,35 @@
+import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { ProductCategory } from '../product-categories/product-category';
+
+import { Product } from './product';
+import { ProductService } from './product.service';
+import { EMPTY, Observable, catchError } from 'rxjs';
+
+@Component({
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class ProductListComponent  {
+  pageTitle = 'Product List';
+  errorMessage = '';
+  categories: ProductCategory[] = [];
+
+  products$ = this.productService.products$
+    .pipe(
+      catchError(err => {
+        this.errorMessage = err;
+        return EMPTY
+      })
+    )
+
+  constructor(private productService: ProductService) { }
+
+  onAdd(): void {
+    console.log('Not yet implemented');
+  }
+
+  onSelected(categoryId: string): void {
+    console.log('Not yet implemented');
+  }
+}
